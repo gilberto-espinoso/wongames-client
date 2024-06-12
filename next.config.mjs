529779b17f3b3f0ce35aa1d1203c34490b39565e
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-12 19:18:41 -0300

    add theme

--- next.config.mjs
+++ next.config.mjs
@@ -3,18 +3,4 @@ const nextConfig = {
   reactStrictMode: true
 }
 
-const isProd = process.env.NODE_ENV === 'production'
-
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')({
-  dest: 'public'
-})
-
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !isProd
-  }
-})
-
 export default nextConfig
