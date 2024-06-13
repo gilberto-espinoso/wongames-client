2e4426a40bf2f83025c69c7cfc413ceca1a29e98
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-13 12:42:24 -0300

    feat: Creating Styles for the Full Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -4,6 +4,8 @@ import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
 import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 
 import Logo from '../../components/Logo'
+import Button from '../../components/Button'
+
 import { useState } from 'react'
 import * as S from './styles'
 import React from 'react'
@@ -29,6 +31,19 @@ const Menu = () => {
       </S.MenuGroup>
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+        <S.MenuNav>
+          <S.MenuLink href="#">Home</S.MenuLink>
+          <S.MenuLink href="#">Explore</S.MenuLink>
+        </S.MenuNav>
+        <S.RegisterBox>
+          <Button fullWidth size="large">
+            Log in now
+          </Button>
+          <span>or</span>
+          <S.CreateAccount href="#" title="Sign Up">
+            Sign Up
+          </S.CreateAccount>
+        </S.RegisterBox>
       </S.MenuFull>
     </S.Wrapper>
   )
