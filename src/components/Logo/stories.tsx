ee0021089341393c74a91c12c47c60abe219c904
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-12 22:14:40 -0300

    feat: add config Logo storybook

--- src/components/Logo/stories.tsx
+++ src/components/Logo/stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Logo from '.'
+import Logo, { LogoProps } from '.'
 
 export default {
   title: 'Logo',
   component: Logo
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<LogoProps> = {}
