cef23480aa452d74925810228f952aec1c164a73
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-13 01:41:19 -0300

    feat: add Heading component

--- src/components/Heading/stories.tsx
+++ src/components/Heading/stories.tsx
@@ -1,9 +1,18 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Heading from '.'
+import Heading, { HeadingProp } from '.'
 
 export default {
   title: 'Heading',
-  component: Heading
+  component: Heading,
+  argTypes: {
+    children: {
+      type: 'string'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<HeadingProp> = {}
+
+Default.args = {
+  children: 'Most Populars'
+}
--- src/components/Heading/styles.ts
+++ src/components/Heading/styles.ts
@@ -1,3 +1,38 @@
-import styled from 'styled-components'
+import styled, { DefaultTheme, css } from 'styled-components'
+import media from 'styled-media-query'
+import { HeadingProp } from '.'
 
-export const Wrapper = styled.main``
+const wrapperModifiers = {
+  lineLeft: (theme: DefaultTheme) => `
+       padding-left: ${theme.spacings.xxsmall};
+       border-left: 0.7rem solid ${theme.colors.secondary};
+    `,
+
+  lineBottom: (theme: DefaultTheme) => `
+       position: relative;
+       margin-bottom: ${theme.spacings.medium};
+
+       &::after {
+        position: absolute;
+        left: 0;
+        bottom: -1rem;
+        content: '';
+        width: 5rem;
+        border: 0.5rem solid ${theme.colors.primary};
+       }
+    `
+}
+
+export const Wrapper = styled.h2<HeadingProp>`
+  ${({ theme, color, lineLeft, lineBottom }) => css`
+    color: ${theme.colors[color!]};
+    font-size: ${theme.font.sizes.xlarge};
+
+    ${media.greaterThan('medium')`
+        font-size: ${theme.font.sizes.xxlarge};
+    `}
+
+    ${lineLeft && wrapperModifiers.lineLeft(theme)};
+    ${lineBottom && wrapperModifiers.lineBottom(theme)};
+  `}
+`
