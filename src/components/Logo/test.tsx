00864578baa02b50048b899790a7bd64fba05c28
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-13 00:03:51 -0300

    feat: Logo add props

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -1,13 +1,18 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
 import Logo from '.'
+import { renderWithTheme } from '@/utils/tests/helpers'
 
 describe('<Logo />', () => {
-  it('should render the heading', () => {
-    const { container } = render(<Logo />)
+  it('should render a white label by default', () => {
+    // Os 3 principais processos de um teste -----
+    // renderizar o componente `render` é o método principal
+    // selecionar o elemento que queremos testar `screen` é o método principal (queries) -getByLabel...
+    // expect - assertion (asserção/comparação/análise) ex: (espero que renderize a logo na cor branca)
 
-    expect(screen.getByRole('heading', { name: /Logo/i })).toBeInTheDocument()
-
-    expect(container.firstChild).toMatchSnapshot()
+    renderWithTheme(<Logo />)
+    expect(screen.getByLabelText(/Won Games/i).parentElement).toHaveStyle({
+      color: '#FAFAFA'
+    })
   })
 })
