7261e3a4a8f75a64f51426767f18eb3ffa63cd7c
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-13 12:01:18 -0300

    Feat(Menu): open/closed menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,28 +1,37 @@
-import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 import { ShoppingCart as ShoppingCartIcon } from '@styled-icons/material-outlined/ShoppingCart'
 import { Search as SearchIcon } from '@styled-icons/material-outlined/Search'
+import { Close as CloseIcon } from '@styled-icons/material-outlined/Close'
+import { Menu2 as MenuIcon } from '@styled-icons/remix-fill/Menu2'
 
 import Logo from '../../components/Logo'
+import { useState } from 'react'
 import * as S from './styles'
 import React from 'react'
 
-const Menu = () => (
-  <S.Wrapper>
-    <S.IconWrapper>
-      <MenuIcon aria-label="Open Menu" />
-    </S.IconWrapper>
-    <S.LogoWrapper>
-      <Logo hideOnMobile aria-label="Wongames" />
-    </S.LogoWrapper>
-    <S.MenuGroup>
-      <S.IconWrapper>
-        <ShoppingCartIcon aria-label="Open Shopping Cart" />
+const Menu = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  return (
+    <S.Wrapper>
+      <S.IconWrapper onClick={() => setIsOpen(true)}>
+        <MenuIcon aria-label="Open Menu" />
       </S.IconWrapper>
-      <S.IconWrapper>
-        <SearchIcon aria-label="Search" />
-      </S.IconWrapper>
-    </S.MenuGroup>
-  </S.Wrapper>
-)
+      <S.LogoWrapper>
+        <Logo hideOnMobile aria-label="Wongames" />
+      </S.LogoWrapper>
+      <S.MenuGroup>
+        <S.IconWrapper>
+          <ShoppingCartIcon aria-label="Open Shopping Cart" />
+        </S.IconWrapper>
+        <S.IconWrapper>
+          <SearchIcon aria-label="Search" />
+        </S.IconWrapper>
+      </S.MenuGroup>
+      <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
+        <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
+      </S.MenuFull>
+    </S.Wrapper>
+  )
+}
 
 export default Menu
