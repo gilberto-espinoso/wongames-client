6f4ee01f9ee13da2b7ffb6f6d68af6e378631ab6
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-15 11:04:53 -0300

    Feat(Highlight): Creating Initial Structure

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -3,7 +3,13 @@ import Highlight from '.'
 
 export default {
   title: 'Highlight',
-  component: Highlight
+  component: Highlight,
+  args: {
+    title: 'Read Dead it’s back',
+    subtitle: 'Come see John’s new adventures',
+    buttonLabel: 'Buy now',
+    buttonLink: '/rdr2'
+  }
 } as Meta
 
 export const Default: StoryObj = {}
