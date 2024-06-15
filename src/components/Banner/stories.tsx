545401783a116a77206dadd1d8f7b4ef37b16d2b
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-14 21:11:26 -0300

    Feat: starting Banner component

---
+++ src/components/Banner/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Banner from '.'
+
+export default {
+  title: 'Banner',
+  component: Banner
+} as Meta
+
+export const Default: StoryObj = {}
