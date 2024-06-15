b8e7204fe1542a026f0e6f17c11e0c32088c0a83
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-14 23:51:44 -0300

    Feat(Ribbon): Finished styling and creation of the storybook

--- src/components/Ribbon/stories.tsx
+++ src/components/Ribbon/stories.tsx
@@ -1,9 +1,21 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Ribbon from '.'
+import Ribbon, { RibbonProps } from '.'
 
 export default {
   title: 'Ribbon',
   component: Ribbon,
+  render: ({ ...args }) => (
+    <div
+      style={{
+        width: '20rem',
+        height: '25rem',
+        position: 'relative',
+        backgroundColor: '#888'
+      }}
+    >
+      <Ribbon {...args} />
+    </div>
+  ),
   args: {
     children: 'Best Seller'
   },
@@ -12,6 +24,6 @@ export default {
       type: 'string'
     }
   }
-} as Meta
+} as Meta<RibbonProps>
 
 export const Default: StoryObj = {}
