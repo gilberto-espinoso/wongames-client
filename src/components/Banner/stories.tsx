b47a461aeccb1004d04317cde3b26a42f455184e
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-15 10:17:52 -0300

    Feat: Banner + Ribbon

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -1,9 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Banner from '.'
+import Banner, { BannerProps } from '.'
 
 export default {
   title: 'Banner',
   component: Banner,
+  render: ({ ...args }) => (
+    <div style={{ maxWidth: '104rem', margin: '0 auto' }}>
+      <Banner {...args} />
+    </div>
+  ),
   args: {
     img: 'https://github.com/gilberto-espinosa/assets/blob/main/games/cyberpunk%20-%20phantom-liberty1042x586.jpg?raw=true',
     title: 'Defy death',
@@ -11,9 +16,14 @@ export default {
     buttonLabel: 'Buy now',
     buttonLink: '/games/defy-death'
   },
+  argTypes: {
+    ribbon: {
+      type: 'string'
+    }
+  },
   parameters: {
     layout: 'fullscreen'
   }
-} as Meta
+} as Meta<BannerProps>
 
 export const Default: StoryObj = {}
