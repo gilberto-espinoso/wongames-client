60e38b702367f6f9858c548bc7b65c192387d2a0
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-14 21:57:37 -0300

    Feat: Starting Ribbon component

---
+++ src/components/Ribbon/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Ribbon from '.'
+
+export default {
+  title: 'Ribbon',
+  component: Ribbon
+} as Meta
+
+export const Default: StoryObj = {}
