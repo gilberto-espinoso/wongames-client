7e0d5e9724627319c78ed94d59d79fa84f6cffe9
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-14 21:39:02 -0300

    Feat(Banner): Creating Initial Structure

--- src/components/Banner/stories.tsx
+++ src/components/Banner/stories.tsx
@@ -3,7 +3,14 @@ import Banner from '.'
 
 export default {
   title: 'Banner',
-  component: Banner
+  component: Banner,
+  args: {
+    img: 'https://github.com/gilberto-espinosa/assets/blob/main/games/cyberpunk%20-%20phantom-liberty1042x586.jpg?raw=true',
+    title: 'Defy death',
+    subtitle: '<p>Play the new <strong>CrashLands</strong> season',
+    buttonLabel: 'Buy now',
+    buttonLink: '/games/defy-death'
+  }
 } as Meta
 
 export const Default: StoryObj = {}
