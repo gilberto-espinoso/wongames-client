4413d85a625a4b4aaec21673cde29583340900d2
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-14 21:47:34 -0300

    Feat(Banner/styles): add Image style

--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,4 +1,5 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+import media from 'styled-media-query'
 
 export const Wrapper = styled.main``
 
@@ -6,7 +7,19 @@ type ImageProps = {
   src: string
 }
 
-export const Image = styled.div<ImageProps>``
+export const Image = styled.div<ImageProps>`
+  ${({ theme, src }) => css`
+    width: 100%;
+    height: 23rem;
+    background-color: ${theme.colors.lightGray};
+    background-image: url(${src});
+    background-position: center center;
+    background-size: cover;
+    ${media.greaterThan('medium')`
+      height: 58rem;
+    `}
+  `}
+`
 
 export const Caption = styled.div``
 
