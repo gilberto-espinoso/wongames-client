fc8d61fb10605dc2a902a0d400b59c48f3be4746
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-15 15:57:02 -0300

    Feat(Slider): Creating Generic Slider and stoybook

--- src/components/Slider/stories.tsx
+++ src/components/Slider/stories.tsx
@@ -1,4 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react'
+import { Settings } from 'react-slick'
+import styled from 'styled-components'
+
 import Slider from '.'
 
 export default {
@@ -6,4 +9,53 @@ export default {
   component: Slider
 } as Meta
 
-export const Default: StoryObj = {}
+const settings: Settings = {
+  dots: true,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1
+}
+
+const verticalSettings: Settings = {
+  vertical: true,
+  verticalSwiping: true,
+  dots: true,
+  infinite: false,
+  slidesToShow: 1
+}
+
+const Slide = styled.div`
+  background: gray;
+  width: 30rem;
+  padding: 10rem 0;
+  border: 0.1rem solid red;
+  color: white;
+  text-align: center;
+`
+
+export const Vertical: StoryObj = {
+  render: () => (
+    <Slider settings={verticalSettings}>
+      <Slide>1</Slide>
+      <Slide>2</Slide>
+      <Slide>3</Slide>
+      <Slide>4</Slide>
+      <Slide>5</Slide>
+      <Slide>6</Slide>
+    </Slider>
+  )
+}
+
+export const Default: StoryObj = {
+  render: () => (
+    <Slider settings={settings}>
+      <Slide>1</Slide>
+      <Slide>2</Slide>
+      <Slide>3</Slide>
+      <Slide>4</Slide>
+      <Slide>5</Slide>
+      <Slide>6</Slide>
+    </Slider>
+  )
+}
