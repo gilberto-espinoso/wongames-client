11335c99b6123d1551cc9efa85e07cc2c57d28b2
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-25 00:05:12 -0300

    Feat: add Sign-in and Sign-up pages

--- src/pages/sign-up.tsx
+++ src/pages/sign-up.tsx
@@ -1,5 +1,10 @@
+import FormSignUp from '@/components/FormSignUp'
 import Auth from '../templates/Auth'
 
 export default function SignIn() {
-  return <Auth title="Sign Up">Sing Up</Auth>
+  return (
+    <Auth title="Sign Up">
+      <FormSignUp />
+    </Auth>
+  )
 }
