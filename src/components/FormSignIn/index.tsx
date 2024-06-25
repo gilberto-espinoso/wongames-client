4400cb8caac024a9832ff9046f8cf076bbc5fab1
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-24 23:32:03 -0300

    feat: add FormSignIn component

---
+++ src/components/FormSignIn/index.tsx
@@ -0,0 +1,39 @@
+import { Email, Lock } from 'styled-icons/material-outlined'
+
+import TextField from '../TextField'
+import Button from '../Button'
+
+import Link from 'next/link'
+import * as S from './styles'
+
+const FormSignIn = () => (
+  <S.Wrapper>
+    <form>
+      <TextField
+        name="email"
+        placeholder="Email"
+        type="email"
+        icon={<Email />}
+      />
+      <TextField
+        name="password"
+        placeholder="Password"
+        type="password"
+        icon={<Lock />}
+      />
+      <S.ForgetPassword href="#">Forgot your password</S.ForgetPassword>
+      <Button size="large" fullWidth>
+        Dign in now
+      </Button>
+
+      <S.FormLink>
+        Don`t have an account?{' '}
+        <Link href="/sign-up">
+          <a>Sign up</a>
+        </Link>
+      </S.FormLink>
+    </form>
+  </S.Wrapper>
+)
+
+export default FormSignIn
