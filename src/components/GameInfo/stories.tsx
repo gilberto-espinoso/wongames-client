fb288190419adb8c6e6b5d36cb247df7dd914aa5
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-26 20:30:51 -0300

    Feat: create GameInfo component

---
+++ src/components/GameInfo/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import GameInfo from '.'
+
+export default {
+  title: 'GameInfo',
+  component: GameInfo
+} as Meta
+
+export const Default: StoryObj = {}
