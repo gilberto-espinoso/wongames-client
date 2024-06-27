7fa6ee24461fc0f053bdafc2866e78cfe9ea9b00
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-26 21:17:20 -0300

    Feat (GameInfo): add story and styles

--- src/components/GameInfo/stories.tsx
+++ src/components/GameInfo/stories.tsx
@@ -1,9 +1,22 @@
 import { Meta, StoryObj } from '@storybook/react'
-import GameInfo from '.'
+import GameInfo, { GameInfoProps } from '.'
+import mockGame from './mock'
 
 export default {
-  title: 'GameInfo',
-  component: GameInfo
+  title: 'Game/GameInfo',
+  component: GameInfo,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  },
+  args: mockGame
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<GameInfoProps> = {
+  render: ({ ...args }) => (
+    <div style={{ maxWidth: '144rem', margin: 'auto', padding: '1.5rem' }}>
+      <GameInfo {...args} />
+    </div>
+  )
+}
--- src/components/MediaMatch/stories.tsx
+++ src/components/MediaMatch/stories.tsx
@@ -7,16 +7,3 @@ export default {
 } as Meta
 
 export const Desktop: StoryObj = {}
-
-// export const Desktop: StoryObj = () => (
-//   <MediaMatch greaterThan="medium">Only on Desktop</MediaMatch>
-// )
-// export const Mobile: StoryObj = () => (
-//   <MediaMatch lessThan="medium">Only on Mobile</MediaMatch>
-// )
-
-// Mobile.parameters = {
-//   viewport: {
-//     defaultViewport: 'mobile1'
-//   }
-// }
