f813b2a35ef8f787dc526121a58f7d1a584d31be
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-26 21:22:35 -0300

    Feat: create GameDetails component

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,20 @@
+import { Meta, StoryObj } from '@storybook/react'
+import GameDetails from '.'
+
+export default {
+  title: 'Game/GameDetails',
+  component: GameDetails,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
+} as Meta
+
+export const Default: StoryObj = {
+  render: () => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+      <GameDetails />
+    </div>
+  )
+}
