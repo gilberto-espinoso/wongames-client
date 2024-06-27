80bd43061e808c20729e3245395546de4b51c1bb
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-06-26 21:54:14 -0300

    Feat (GameDetails): add storybook

--- src/components/GameDetails/stories.tsx
+++ src/components/GameDetails/stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react'
-import GameDetails from '.'
+import GameDetails, { GameDetailsProps } from '.'
 
 export default {
   title: 'Game/GameDetails',
@@ -8,13 +8,24 @@ export default {
     backgrounds: {
       default: 'won-dark'
     }
+  },
+  args: {
+    platforms: ['windows', 'linux', 'mac']
+  },
+  argTypes: {
+    platforms: {
+      control: {
+        type: 'radio',
+        options: ['windows', 'linux', 'mac']
+      }
+    }
   }
 } as Meta
 
-export const Default: StoryObj = {
-  render: () => (
+export const Default: StoryObj<GameDetailsProps> = {
+  render: ({ ...args }) => (
     <div style={{ maxWidth: '130rem', margin: '0 auto' }}>
-      <GameDetails platforms={['windows', 'linux', 'mac']} />
+      <GameDetails platforms={args.platforms} />
     </div>
   )
 }
