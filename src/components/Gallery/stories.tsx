3c68444a5b59e05f1c2bf02944ee7e89798a6887
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-19 21:56:47 -0300

    Feat: created Gallery component

---
+++ src/components/Gallery/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Gallery from '.'
+
+export default {
+  title: 'Gallery',
+  component: Gallery
+} as Meta
+
+export const Default: StoryObj = {}
