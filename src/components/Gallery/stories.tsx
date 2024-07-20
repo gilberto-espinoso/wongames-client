27be10f80f34bddd23d6a39bf4c33450aa0d33fd
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-19 22:17:26 -0300

    Feat(Gallery): Structure Created

--- src/components/Gallery/stories.tsx
+++ src/components/Gallery/stories.tsx
@@ -1,9 +1,25 @@
 import { Meta, StoryObj } from '@storybook/react'
-import Gallery from '.'
+import Gallery, { GalleryProps } from '.'
+import items from './mock'
 
 export default {
   title: 'Gallery',
-  component: Gallery
+  component: Gallery,
+  args: {
+    items
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<GalleryProps> = {
+  render: ({ ...args }) => (
+    <div style={{ maxWidth: '130rem', margin: '0 auto', padding: '5rem' }}>
+      <Gallery {...args} />
+    </div>
+  )
+}
