7069f3880f39a36f61ca9a84919ce391a575d83b
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-21 13:17:39 -0300

    Feat (template/Game): add cover image

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,10 +1,14 @@
 import Base from '../../templates/Base'
 
-// import * as S from './styles'
+import * as S from './styles'
 
 const Game = () => (
   <Base>
-    <h1>Game</h1>
+    <S.Cover
+      src="https://images.gog-statics.com/031b5010e665b8794be92683cfd3e03fa8d0cf9aa4bb2755055f0e4fb3381c8f.jpg"
+      role="image"
+      arial-label="cover"
+    />
   </Base>
 )
 
