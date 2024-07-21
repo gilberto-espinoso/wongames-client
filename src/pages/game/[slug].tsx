914635fe16c464d58c082077bc369b49788f817d
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-21 19:08:15 -0300

    Feat (template/Game): add getStaticProps and getStaticPaths

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -1,5 +1,22 @@
-import Game from '../../templates/Game'
+import Game, { GameTemplateProps } from '../../templates/Game'
 
-export default function Index() {
-  return <Game />
+export default function Index(props: GameTemplateProps) {
+  return <Game {...props} />
+}
+
+// gerar em build time
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { slug: 'baldur-gate-3' } }],
+    fallback: false
+  }
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      cover:
+        'https://images.gog-statics.com/031b5010e665b8794be92683cfd3e03fa8d0cf9aa4bb2755055f0e4fb3381c8f.jpg'
+    }
+  }
 }
--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -2,13 +2,13 @@ import Base from '../../templates/Base'
 
 import * as S from './styles'
 
-const Game = () => (
+export type GameTemplateProps = {
+  cover: string
+}
+
+const Game = ({ cover }: GameTemplateProps) => (
   <Base>
-    <S.Cover
-      src="https://images.gog-statics.com/031b5010e665b8794be92683cfd3e03fa8d0cf9aa4bb2755055f0e4fb3381c8f.jpg"
-      role="image"
-      arial-label="cover"
-    />
+    <S.Cover src={cover} role="image" arial-label="cover" />
   </Base>
 )
 
