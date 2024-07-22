1c2300b87b7091c5e86d8d0cafb8c4816ab54b92
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-22 20:07:54 -0300

    Feat (Menu): add home link

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -26,13 +26,18 @@ const Menu = ({ username }: MenuProps) => {
           <MenuIcon aria-label="Open Menu" />
         </S.IconWrapper>
       </MediaMatch>
+
       <S.LogoWrapper>
-        <Logo hideOnMobile aria-label="Wongames" />
+        <Link href="/" passHref>
+          <Logo hideOnMobile aria-label="Wongames" />
+        </Link>
       </S.LogoWrapper>
 
       <MediaMatch greaterThan="medium">
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
         </S.MenuNav>
       </MediaMatch>
@@ -52,10 +57,13 @@ const Menu = ({ username }: MenuProps) => {
           </MediaMatch>
         )}
       </S.MenuGroup>
+
       <S.MenuFull aria-hidden={!isOpen} isOpen={isOpen}>
         <CloseIcon aria-label="Close Menu" onClick={() => setIsOpen(false)} />
         <S.MenuNav>
-          <S.MenuLink href="#">Home</S.MenuLink>
+          <Link href="/" passHref>
+            <S.MenuLink>Home</S.MenuLink>
+          </Link>
           <S.MenuLink href="#">Explore</S.MenuLink>
           {!!username && (
             <>
