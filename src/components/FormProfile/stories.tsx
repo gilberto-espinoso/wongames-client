3ae31eca5ffcfffb2cabe14e3be04882f952db49
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-28 20:04:02 -0300

    Feat: Create FormProfile component

---
+++ src/components/FormProfile/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import FormProfile from '.'
+
+export default {
+  title: 'FormProfile',
+  component: FormProfile
+} as Meta
+
+export const Default: StoryObj = {}
