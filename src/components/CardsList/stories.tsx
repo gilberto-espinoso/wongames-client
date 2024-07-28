373c9fc70ac655060abc2bbb15d5645b5b4fbfe7
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-28 20:15:17 -0300

    Feat: Create CardsList component

---
+++ src/components/CardsList/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import CardsList from '.'
+
+export default {
+  title: 'CardsList',
+  component: CardsList
+} as Meta
+
+export const Default: StoryObj = {}
