ce91ffcac002e5b09629f14b25aeff4178ae86bd
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-28 20:50:08 -0300

    Feat: Create OrdersList componente

---
+++ src/components/OrdersList/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import OrdersList from '.'
+
+export default {
+  title: 'OrdersList',
+  component: OrdersList
+} as Meta
+
+export const Default: StoryObj = {}
