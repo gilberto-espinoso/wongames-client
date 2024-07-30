66d7bb16de099d282c0e81879d3aa63a2c8e06c6
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-29 22:10:35 -0300

    Feat (ExplorerSidebar): add structure and styles

--- src/components/ExploreSidebar/stories.tsx
+++ src/components/ExploreSidebar/stories.tsx
@@ -1,9 +1,37 @@
 import { Meta, StoryObj } from '@storybook/react'
-import ExploreSidebar from '.'
+import ExploreSidebar, { ExploreSidebarProps } from '.'
+
+import items from './mock'
 
 export default {
   title: 'ExploreSidebar',
-  component: ExploreSidebar
+  component: ExploreSidebar,
+  args: {
+    items
+  },
+  parameters: {
+    layout: 'fullscreen',
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<ExploreSidebarProps> = {
+  render: ({ ...args }) => (
+    <div style={{ padding: 16, maxWidth: 320 }}>
+      <ExploreSidebar {...args} />
+    </div>
+  )
+}
+
+export const WithInitialValues: StoryObj<ExploreSidebarProps> = {
+  render: ({ ...args }) => (
+    <div style={{ padding: 16, maxWidth: 320 }}>
+      <ExploreSidebar
+        {...args}
+        initialValues={{ windows: true, sort_by: 'low-to-high' }}
+      />
+    </div>
+  )
+}
