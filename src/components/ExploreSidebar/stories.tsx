5bd1a9e0a4b25514576ed092796f8d0083e46286
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-29 21:53:02 -0300

    Feat: create ExplorerSidebar component

---
+++ src/components/ExploreSidebar/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import ExploreSidebar from '.'
+
+export default {
+  title: 'ExploreSidebar',
+  component: ExploreSidebar
+} as Meta
+
+export const Default: StoryObj = {}
