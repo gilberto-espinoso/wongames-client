f75343878c112a37d17c2d10fa0fa3a5e0f56dec
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 23:49:41 -0300

    Feat (UserDropdown): Finalized

--- src/components/UserDropdown/stories.tsx
+++ src/components/UserDropdown/stories.tsx
@@ -1,9 +1,26 @@
 import { Meta, StoryObj } from '@storybook/react'
-import UserDropdown from '.'
+import UserDropdown, { UserDropdownProps } from '.'
 
 export default {
   title: 'UserDropdown',
-  component: UserDropdown
+  component: UserDropdown,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<UserDropdownProps> = {
+  render: ({ ...args }) => (
+    <div
+      style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}
+    >
+      <UserDropdown {...args} />
+    </div>
+  )
+}
+
+Default.args = {
+  username: 'Wanner'
+}
