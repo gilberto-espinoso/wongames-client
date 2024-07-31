91753d472e432365f942b869ab7f435912dc733b
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 22:33:04 -0300

    Feat: Create CartDropdown component

---
+++ src/components/CartDropdown/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import CartDropdown from '.'
+
+export default {
+  title: 'CartDropdown',
+  component: CartDropdown
+} as Meta
+
+export const Default: StoryObj = {}
