8fe4e3332c9e4663443716febeff2b75f6a4ae5a
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 23:05:52 -0300

    Feat: Create UserDropdown

---
+++ src/components/UserDropdown/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import UserDropdown from '.'
+
+export default {
+  title: 'UserDropdown',
+  component: UserDropdown
+} as Meta
+
+export const Default: StoryObj = {}
