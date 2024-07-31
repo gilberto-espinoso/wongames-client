e656e081ba8165a202a1c6e14d895c0a1e04cd86
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 22:45:00 -0300

    Feat: CartDropdown Finalized

--- src/components/CartDropdown/stories.tsx
+++ src/components/CartDropdown/stories.tsx
@@ -1,9 +1,28 @@
 import { Meta, StoryObj } from '@storybook/react'
-import CartDropdown from '.'
+import CartDropdown, { CartDropdownProps } from '.'
+
+import items from '@/components/CartList/mock'
 
 export default {
   title: 'CartDropdown',
-  component: CartDropdown
+  component: CartDropdown,
+  args: {
+    items,
+    total: 'R$ 300,00'
+  },
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<CartDropdownProps> = {
+  render: ({ ...args }) => (
+    <div
+      style={{ maxWidth: '98%', display: 'flex', justifyContent: 'flex-end' }}
+    >
+      <CartDropdown {...args} />
+    </div>
+  )
+}
