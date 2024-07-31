17e4a9825860348434fb6742a783c370558cd254
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 22:06:19 -0300

    Feat (CartIcon): add style

--- src/components/CartIcon/stories.tsx
+++ src/components/CartIcon/stories.tsx
@@ -1,9 +1,14 @@
 import { Meta, StoryObj } from '@storybook/react'
-import CartIcon from '.'
+import CartIcon, { CartIconProps } from '.'
 
 export default {
   title: 'CartIcon',
-  component: CartIcon
+  component: CartIcon,
+  parameters: {
+    backgrounds: {
+      default: 'won-dark'
+    }
+  }
 } as Meta
 
-export const Default: StoryObj = {}
+export const Default: StoryObj<CartIconProps> = {}
