b7abb178af64d29efe959910e6ae01c7fc7b2a0b
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 21:57:37 -0300

    Feat: Create CartIcon component

---
+++ src/components/CartIcon/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import CartIcon from '.'
+
+export default {
+  title: 'CartIcon',
+  component: CartIcon
+} as Meta
+
+export const Default: StoryObj = {}
