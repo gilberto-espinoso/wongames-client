db0d6b99ddeb4dd2587d0262f6ac74ad58088aaf
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 22:08:37 -0300

    Feat: create Dropdown component

---
+++ src/components/Dropdown/stories.tsx
@@ -0,0 +1,9 @@
+import { Meta, StoryObj } from '@storybook/react'
+import Dropdown from '.'
+
+export default {
+  title: 'Dropdown',
+  component: Dropdown
+} as Meta
+
+export const Default: StoryObj = {}
