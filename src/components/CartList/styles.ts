0678238e10a9e78024b1e459c04d291bc8e69c58
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-07-30 23:00:12 -0300

    Refactor (CartList): add has empty

--- src/components/CartList/styles.ts
+++ src/components/CartList/styles.ts
@@ -2,15 +2,37 @@ import { tint } from 'polished'
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
 
-export const Wrapper = styled.main`
-  ${({ theme }) => css`
+import * as EmptyStyles from '@/components/Empty/styles'
+
+type WrapperProps = {
+  isEmpty: boolean
+}
+
+export const Wrapper = styled.div<WrapperProps>`
+  ${({ theme, isEmpty }) => css`
     background: ${theme.colors.white};
     display: flex;
-    align-items: center;
     flex-direction: column;
     align-self: start;
+    ${isEmpty &&
+    css`
+      ${EmptyStyles.Wrapper} {
+        padding-bottom: ${theme.spacings.medium};
+      }
+      ${EmptyStyles.Image} {
+        max-width: 20rem;
+      }
+      ${EmptyStyles.Title} {
+        font-size: ${theme.font.sizes.large};
+      }
+      ${EmptyStyles.Description} {
+        color: ${theme.colors.black};
+        font-size: ${theme.font.sizes.medium};
+      }
+    `}
   `}
 `
+
 export const Footer = styled.div`
   ${({ theme }) => css`
     background: ${tint(0.2, theme.colors.lightGray)};
