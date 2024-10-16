de7ed17aa03f693bd01905cd445107675b9f6d91
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-10-16 10:46:35 -0300

    Feat - SSG validate

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,15 +1,62 @@
 import GamesTemplate, { GamesTemplateProps } from '@/templates/Games'
 import filterItemsMock from '@/components/ExploreSidebar/mock'
-import gamesMock from '@/components/GameCardSlider/mock'
+import { initializeApollo } from '@/utils/apollo'
+import { gql } from '@apollo/client'
 
 export default function GamesPage(props: GamesTemplateProps) {
   return <GamesTemplate {...props} />
 }
 
-export async function getServerSideProps() {
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({
+    query: gql`
+      query QueryGames {
+        games {
+          data {
+            attributes {
+              name
+              slug
+              cover {
+                data {
+                  attributes {
+                    url
+                  }
+                }
+              }
+              develops {
+                data {
+                  attributes {
+                    name
+                  }
+                }
+              }
+              price
+            }
+          }
+        }
+      }
+    `
+  })
+
   return {
     props: {
-      games: gamesMock,
+      revalidate: 60,
+      games: data.games.data.map((game) => ({
+        title: game.attributes.name,
+        developer:
+          game.attributes.develops?.data[0]?.attributes?.name ||
+          'Unknown Developer',
+        img:
+          `http://localhost:1337${game.attributes.cover?.data?.attributes?.url}` ||
+          null,
+        price:
+          new Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(game.attributes.price) || 0
+      })),
       filterItems: filterItemsMock
     }
   }
