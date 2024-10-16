89e91ccce1029d06a37b2b20b7ffce7306519eff
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-10-16 07:52:41 -0300

    Feat - base apollo config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,28 @@
-import { ApolloClient, InMemoryCache, gql } from '@apollo/client'
+import { gql, useQuery } from '@apollo/client'
 
-import Home, { HomeTemplateProps } from '../templates/Home'
 import bannersMock from '../components/BannerSlider/mock'
 import gamesMock from '../components/GameCardSlider/mock'
 import highlightMock from '../components/Highlight/mock'
+import Home, { HomeTemplateProps } from '../templates/Home'
 
 export default function Index(props: HomeTemplateProps) {
-  const client = new ApolloClient({
-    uri: 'http://localhost:1337/graphql',
-    cache: new InMemoryCache()
-  })
-
-  client.query({
-    query: gql`
-      query getGames {
-        games {
-          data {
-            attributes {
-              name
-            }
+  const { data, loading, error } = useQuery(gql`
+    query getGames {
+      games {
+        data {
+          attributes {
+            name
           }
         }
       }
-    `
-  })
+    }
+  `)
+
+  if (loading) return <p>Carregando... </p>
+
+  if (error) return <p>{error.message}</p>
+
+  if (data) return <p>{JSON.stringify(data, null, 2)}</p>
 
   return <Home {...props} />
 }
