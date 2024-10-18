54287bb603304c90f6d9f155f6adfeda3cb72cef
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-10-17 22:29:49 -0300

    Feat - integrating banner on home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,32 @@
-import bannersMock from '../components/BannerSlider/mock'
+import { initializeApollo } from '@/utils/apollo'
 import gamesMock from '../components/GameCardSlider/mock'
 import highlightMock from '../components/Highlight/mock'
 import Home, { HomeTemplateProps } from '../templates/Home'
+import { QueryHome } from '@/graphql/generated/QueryHome'
+import { QUERY_HOME } from '@/graphql/queries/home'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
 }
 
-export async function getServerSideProps() {
-  //retorno dos dados
+export async function getStaticProps() {
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query<QueryHome>({ query: QUERY_HOME })
+
   return {
     props: {
-      banners: bannersMock,
+      revalidate: 20,
+      banners: data.banners.data.map((banner) => ({
+        img: `http://localhost:1337${banner.attributes?.image.data?.attributes?.url}`,
+        title: banner.attributes?.title || null,
+        subtitle: banner.attributes?.subtitle || null,
+        buttonLabel: banner.attributes?.button?.label || null,
+        buttonLink: banner.attributes?.button?.link || null,
+        ribbon: banner.attributes?.ribbon?.text || null,
+        ribbonColor: banner.attributes?.ribbon?.color || null,
+        ribbonSize: banner.attributes?.ribbon?.size || null
+      })),
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
       mostPopularGames: gamesMock,
