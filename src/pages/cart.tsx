c5928fc102cb0569594aaf7c7cd09474245d2991
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-10-20 10:53:51 -0300

    Feat - add recommended games in cart

--- src/pages/cart.tsx
+++ src/pages/cart.tsx
@@ -1,22 +1,42 @@
 import Cart, { CartProps } from '@/templates/Cart'
 
-import itemsMock from '@/components/CartList/mock'
 import gamesMock from '@/components/GameCardSlider/mock'
-import higlightMock from '@/components/Highlight/mock'
-import cardsMock from '@/components/PaymentOptions/mock'
+import { QueryRecommended } from '@/graphql/generated/QueryRecommended'
+import { QUERY_RECOMMENDED } from '@/graphql/queries/recommended'
+import { initializeApollo } from '@/utils/apollo'
+import { gamesMapper } from '@/utils/mappers'
 
 export default function CartPage(props: CartProps) {
   return <Cart {...props} />
 }
 
 export async function getServerSideProps() {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query<QueryRecommended>({
+    query: QUERY_RECOMMENDED
+  })
+
   return {
     props: {
-      items: itemsMock,
-      total: '$ 430,00',
-      cards: cardsMock,
-      recommendedGames: gamesMock,
-      recommendedHighlight: higlightMock
+      revalidate: 20,
+      games: gamesMock,
+      recommendedTitle: data.recommended.data?.attributes?.section?.title,
+      recommendedGames: gamesMapper(
+        data.recommended.data?.attributes?.section?.games.data
+      ),
+      recommendedHighlight: {
+        title: data.recommended.data?.attributes?.section?.highlight?.title,
+        subtitle:
+          data.recommended.data?.attributes?.section?.highlight?.subtitle,
+        backgroundImage: `http://localhost:1337${data.recommended.data?.attributes?.section?.highlight?.background?.data?.attributes?.url}`,
+        floatImage: `http://localhost:1337${data.recommended.data?.attributes?.section?.highlight?.floatImage?.data?.attributes?.url}`,
+        buttonLabel:
+          data.recommended.data?.attributes?.section?.highlight?.buttonLabel,
+        buttonLink:
+          data.recommended.data?.attributes?.section?.highlight?.buttonLink,
+        alignment:
+          data.recommended.data?.attributes?.section?.highlight?.alignment
+      }
     }
   }
 }
