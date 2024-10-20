a8f16c2044d8a46a8c12ae6aac5bc5e6d2b429bd
gilberto-espinosa <gilbertosilvaespinosa@gmail.com>
2024-10-20 13:07:36 -0300

    Feat - templates/Games:  297-useQuery/ add useQuery

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,18 +1,25 @@
-import Base from '@/templates/Base'
+import { useQuery } from '@apollo/client'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 
 import ExploreSidebar, { ItemProps } from '@/components/ExploreSidebar'
 import GameCard, { GameCardProps } from '@/components/GameCard'
 import { Grid } from '@/components/Grid'
+import Base from '@/templates/Base'
 
 import * as S from './styles'
+import { QueryGames, QueryGamesVariables } from '@/graphql/generated/QueryGames'
+import { QUERY_GAMES } from '@/graphql/queries/games'
 
 export type GamesTemplateProps = {
   games?: GameCardProps[]
   filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { data } = useQuery<QueryGames, QueryGamesVariables>(QUERY_GAMES, {
+    variables: { limit: 9 }
+  })
+
   const handleFilter = () => {
     return
   }
@@ -28,8 +35,15 @@ const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
 
         <section>
           <Grid>
-            {games.map((item) => (
-              <GameCard key={item.title} {...item} />
+            {data?.games.data.map((game) => (
+              <GameCard
+                key={game.attributes.slug}
+                title={game.attributes.name}
+                slug={game.attributes.slug}
+                developer={game.attributes.develops?.data[0]?.attributes?.name}
+                img={`http://localhost:1337${game.attributes.cover?.data?.attributes?.url}`}
+                price={game.attributes.price}
+              />
             ))}
           </Grid>
 
